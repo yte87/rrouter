453faa048009a5c33261062fea6a244ec1bdf346
yeti <y3t1mk@gmail.com>
2020-05-19 22:54:14 +0200

    Updates

---
+++ precache-manifest.93dd031e30b0296a097c415d36b04b5d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f9656d7c9ba6a903d311d38acfc3c58",
+    "url": "/rrouter/index.html"
+  },
+  {
+    "revision": "541db6a7c91f74181dca",
+    "url": "/rrouter/static/css/main.2988f928.chunk.css"
+  },
+  {
+    "revision": "f679bc3a86491b70c179",
+    "url": "/rrouter/static/js/2.126121ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rrouter/static/js/2.126121ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "541db6a7c91f74181dca",
+    "url": "/rrouter/static/js/main.0d851568.chunk.js"
+  },
+  {
+    "revision": "03732659c224355eea9c",
+    "url": "/rrouter/static/js/runtime-main.635eb0fe.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/rrouter/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/rrouter/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/rrouter/static/media/header3.d9d36b65.jpg"
+  }
+]);
